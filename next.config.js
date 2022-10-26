a1fb878fee3828882519057841fee5ae079087c1
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2022-10-26 13:12:01 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  swcMinify: true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const path = require('path');
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  }
+};
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer({});
+
+{
+  images: {
+    formats: ['image/avif', 'image/webp'];
+  }
+}
