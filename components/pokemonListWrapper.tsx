3a6e914f13a3934738caf47fd826a5e9126baabd
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2022-12-12 11:21:17 +0530

    Update pokemonListWrapper.tsx
    kuch kiya list mein

--- components/pokemonListWrapper.tsx
+++ components/pokemonListWrapper.tsx
@@ -1,3 +1,5 @@
 import { PokemonList } from '../pages/api/pokemons';
 
-export default function PokemonListWrapper(pokemons: PokemonList[]) {}
+export default function PokemonListWrapper({pokemons}: {pokemons: PokemonList[]}) {
+  console.log(pokemons);
+}
