6ca2138ec721b88513354ba0b2831ddd08534449
deveshwar.jaiswal <deveshwar.jaiswal996@gmail.com>
2022-12-12 15:46:27 +0530

    added infinite loader, wrapper

--- components/pokemonListWrapper.tsx
+++ components/pokemonListWrapper.tsx
@@ -1,5 +1,26 @@
 import { PokemonList } from '../pages/api/pokemons';
+import s from '../styles/pokemonListWrapper.module.scss';
+import cn from 'clsx'
 
-export default function PokemonListWrapper({pokemons}: {pokemons: PokemonList[]}) {
-  console.log(pokemons);
+export default function PokemonListWrapper({
+  pokemons
+}: {
+  pokemons: PokemonList[];
+}) {
+
+  return (
+    <>
+      <div className={cn(s.mainGrid)}>
+        {
+          pokemons.map((item: PokemonList) => (
+            <div key={item.id} className={cn(s.box)}>
+              {item.name}
+            </div>
+          ))
+        }
+      </div>
+
+
+    </>
+  );
 }
--- pages/pokemon/index.tsx
+++ pages/pokemon/index.tsx
@@ -1,33 +1,58 @@
 import { useEffect, useState } from 'react';
+import PokemonListWrapper from '../../components/pokemonListWrapper';
 import getPokemons, { PokemonList, PokemonListQuery } from '../api/pokemons';
 
+import cn from 'clsx';
+
 export default function Pokemon() {
-  const [pokemons, setPokemons] = useState({ pokemons: [] as PokemonList[] });
+  const [pokemons, setPokemons] = useState<PokemonList[]>([]);
   const [isLoading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
   const query: PokemonListQuery = { type: `pokemon`, limit: 20, offset: 0 };
   useEffect(() => {
     setLoading(true);
-    getPokemons(query).then(
-      res => {
-        console.log(res);
-        
-//         pokemons.pokemons = [...pokemons.pokemons, ...res.list];
-//         setPokemons(pokemons);
-        setPokemons( (prev) => [...prev, ...res.list] )
-        setLoading(false);
-      },
-      err => {
-        console.error(err);
-        setLoading(false);
-      }
-    );
+    getDataFromAPI();
   }, []);
-  return (
-    <div className="wrapper">
-      <h1>Hi from pokemon page</h1>
-      
-      <PokemonListWrapper data={pokemons} />
-    </div>
-  );
+
+ const getDataFromAPI = () => {
+   getPokemons(query).then(
+     res => {
+       // @ts-ignore
+       setPokemons(prev => [...prev, ...res.list]);
+     },
+     err => {
+       console.error(err);
+     }
+   ).catch(e => {
+     console.error(e);
+   }).finally(() => {
+     setLoading(false);
+   });
+ }
+ const loadMore =() => {
+    query.offset += query.limit;
+   getDataFromAPI();
+  }
+  if (isLoading) {
+    return (
+      <>
+      <h1>Loading Data...</h1>
+    </>
+    );
+  } else {
+    return (
+      <div className="wrapper">
+        <h1 className={cn('mb-4')}>Hi from pokemon page</h1>
+
+        <PokemonListWrapper  pokemons={pokemons}/>
+        <div className={cn('flex justify-center my-4')}>
+          <button className={cn('bg-red-600 text-white font-montserrat py-2 px-8 font-medium rounded-xl hover:bg-red-500 transition-all duration-300 load-more-btn')} onClick={loadMore}>Load More</button>
+        </div>
+
+
+      </div>
+    );
+  }
+
 }
 // href={pathname: /pokemon/${pokemon.id}, query: {name: 'lll'}}
