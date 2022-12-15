ee981428d5f1f16fd7121932d01879cadb75662d
deveshwar.jaiswal <deveshwar.jaiswal996@gmail.com>
2022-12-15 23:31:17 +0530

    BUTTON UI

--- components/pdfComponent.tsx
+++ components/pdfComponent.tsx
@@ -49,24 +49,24 @@ export default function PdfComponent({pdf
         <Outline onItemClick={onItemClick} />
         <Page scale={4} renderTextLayer={false} renderAnnotationLayer={false} height={600} width={450} pageNumber={pageNumber} />
       </Document>
-      <div>
-        <p>
+      <div className={'more-btn-stuff'}>
+        <p className={'py-2 m-0'}>
           Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
         </p>
-        <button
-          type="button"
-          disabled={pageNumber <= 1}
-          onClick={previousPage}
-        >
-          Previous
-        </button>
-        <button
-          type="button"
-          disabled={pageNumber >= numPages}
-          onClick={nextPage}
-        >
-          Next
-        </button>
+        <div className={cn('btn-section py-2')}>
+          <button  type="button"
+                   disabled={pageNumber <= 1}
+                   onClick={previousPage}
+                   className="next-prev-btn mr-2 bg-indigo-600 text-white font-montserrat py-2 px-8 font-medium rounded-xl hover:bg-indigo-500 transition-all duration-300"
+          > {'<'}
+          </button>
+          <button   type="button"
+                    disabled={pageNumber >= numPages}
+                    onClick={nextPage}
+                    className="next-prev-btn bg-indigo-600 text-white font-montserrat py-2 px-8 font-medium rounded-xl hover:bg-indigo-500 transition-all duration-300"
+          > {'>'}
+          </button>
+        </div>
       </div>
     </div>
   );
--- pages/pdf/index.tsx
+++ pages/pdf/index.tsx
@@ -4,7 +4,7 @@ import PdfComponent from '../../components/pdfComponent';
 export default function PdfViewer() {
 return (
   <>
-    <PdfComponent pdf='https://d2bps9p1kiy4ka.cloudfront.net/5eb393ee95fab7468a79d189/b22e7ac8-c276-418c-ad63-fdb69c1e90fc.pdf'/>
+    <PdfComponent pdf='https://d2bps9p1kiy4ka.cloudfront.net/5eb393ee95fab7468a79d189/95ac8a83-d529-4847-8c97-0afa8c7a504c.pdf'/>
   </>
 )
 
